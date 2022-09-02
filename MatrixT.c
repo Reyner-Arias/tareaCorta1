68333759c1f8b3a63ee2aa7a611d68caec2ab939
Reyner-Arias <Rey86581@gmail.com>
2022-09-02 14:31:08 -0600

    MatrixT Finish

--- MatrixT.c
+++ MatrixT.c
@@ -4,15 +4,13 @@
 #include <time.h>
 
 struct matrix {
-    int* row;
-    int* column;
-    int* totales;
     int thread;
 };
 
+int **matriz1, **matriz2, **matrizRes;
 int n = 0;
 
-void rellenarMatriz(int matriz[][n]){
+void rellenarMatriz(int** matriz){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             matriz[i][j] = rand() % 10 - 4;
@@ -20,77 +18,83 @@ void rellenarMatriz(int matriz[][n]){
     }
 }
 
+void rellenarCeros(int** matriz){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            matriz[i][j] = 0;
+        }
+    }
+}
+
 void *rowXColumn(void *datosP)
 {
     struct matrix *datos = datosP;
-    datos->totales[datos->thread] = 0;
     for(int i = 0; i < n; i++){
-        datos->totales[datos->thread] += datos->row[i]*datos->column[i];
+        for(int j = 0; j < n; j++){
+            matrizRes[datos->thread][i] += matriz1[datos->thread][j]*matriz2[j][i];
+        }
     }
 }
 
 int main(){
     time_t t1;
-    srand ( (unsigned) time (&t1)); 
+    srand ( (unsigned) time (&t1));
     double promedio;
 
     printf("Digite el número que definirá la cantidad de filas y columnas de las matrices cuadradas aleatorias\n");
     scanf("%d", &n);
+    double tiempos[n];
 
-    if(n>0&&n<=180){
-        for(int rep = 0; rep < 100; rep++){
-            int matriz1[n][n];
-            int matriz2[n][n];
-            int matriz2T[n][n];
-            int matrizRes[n][n];
+    for(int rep = 0; rep < 100; rep++){
+        
+        matriz1 = malloc(n * sizeof *matriz1);
+        matriz2 = malloc(n * sizeof *matriz2);
+        matrizRes = malloc(n * sizeof *matrizRes);
 
-            double executionTime = 0.0;
-
-            rellenarMatriz(matriz1);
-            rellenarMatriz(matriz2);
-
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    matriz2T[i][j] = matriz2[j][i];
-                }
-            }
-
-            clock_t begin = clock();
-
-            pthread_t threads[n*n];
-            struct matrix datosHilos[n*n];
-            int totales[n*n];
-            int thread = 0;
-
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    
-                    datosHilos[thread].row = matriz1[i];
-                    datosHilos[thread].column = matriz2T[j];
-                    datosHilos[thread].totales = totales;
-                    datosHilos[thread].thread = thread;
-                    
-                    pthread_create( &threads[thread], NULL, rowXColumn, &datosHilos[thread]);
-                    thread++;
-                }
-            }
-
-            for(int i = 0; i < n*n; i++){
-                pthread_join( threads[i], NULL);
-            }
-            for(int i = 0; i < n*n; i++){
-                matrizRes[(i-(i%n))/n][i%n] = totales[i];
-            }
-
-            clock_t end = clock();
-            executionTime += (double)(end - begin) / CLOCKS_PER_SEC;
-            promedio += executionTime;
+        for(int i = 0; i < n; i++){
+            matriz1[i] = malloc(n * sizeof *matriz1[i]);
+            matriz2[i] = malloc(n * sizeof *matriz2[i]);
+            matrizRes[i] = malloc(n * sizeof *matrizRes[i]);
         }
-        promedio = promedio/100;
-        printf("El tiempo promedio fue de %f segundos\n", promedio);
-    } else {
-        printf("Matrices con un n > 180 o n < 1, no están permitidas\n");
+
+        double executionTime = 0.0;
+
+        rellenarMatriz(matriz1);
+        rellenarMatriz(matriz2);
+        rellenarCeros(matrizRes);
+
+        clock_t begin = clock();
+
+        pthread_t threads[n];
+        struct matrix datosHilos[n];
+        int thread = 0;
+
+        for(int i = 0; i < n; i++){
+            datosHilos[thread].thread = thread;
+            pthread_create( &threads[thread], NULL, rowXColumn, &datosHilos[thread]);
+            thread++;
+        }
+
+        for(int i = 0; i < n; i++){
+            pthread_join( threads[i], NULL);
+        }
+
+        clock_t end = clock();
+        executionTime += (double)(end - begin) / CLOCKS_PER_SEC;
+        tiempos[thread] = executionTime;
+        promedio += executionTime;
     }
+    promedio = promedio/100;
+    printf("El tiempo promedio fue de %f segundos\n", promedio);
+    
+    for(int i = 0; i < n; i++){
+        free(matriz1[i]);
+        free(matriz2[i]);
+        free(matrizRes[i]);
+    }
+    free(matriz1);
+    free(matriz2);
+    free(matrizRes);
 
     return 0;
 }
